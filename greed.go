e2963e5d4a6b14b5cddbedfa69ae32e204965703
Franco Lombardo <f dot lombardo 69 at gmail dot com>
2016-08-15 17:21:15 +0200

    First release

---
+++ greed.go
@@ -0,0 +1,53 @@
+//Greed dice game kata.
+package greedgo
+
+import (
+	"sort"
+)
+
+type Roll []int
+
+func (roll Roll) startsWith(anotherRoll ...int) bool {
+	if len(anotherRoll) > len(roll) {
+		return false
+	}
+
+	for i, value:= range anotherRoll {
+		if roll[i] != value {
+			return false
+		}
+	}
+
+	return true
+}
+
+func Greed(dice []int) int {
+	sort.Ints(dice)
+	score := 0
+	for ; len(dice) > 0; {
+		score, dice = compute(score, dice)
+	}
+	return score
+}
+
+func compute(score int, sortedDice Roll) (int, Roll) {
+	switch {
+		case sortedDice.startsWith(1, 1, 1):
+			return score + 1000, sortedDice[3:]
+		case sortedDice.startsWith(1):
+			return score + 100, sortedDice[1:]
+		case sortedDice.startsWith(2, 2, 2):
+			return score + 100 * 2, sortedDice[3:]		
+		case sortedDice.startsWith(3, 3, 3):
+			return score + 100 * 3, sortedDice[3:]
+		case sortedDice.startsWith(4, 4, 4):
+			return score + 100 * 4, sortedDice[3:]
+		case sortedDice.startsWith(5, 5, 5):
+			return score + 100 * 5, sortedDice[3:]						
+		case sortedDice.startsWith(6, 6, 6):
+			return score + 100 * 6, sortedDice[3:]				
+		case sortedDice.startsWith(5):
+			return score + 50, sortedDice[1:]								
+	}
+	return score, sortedDice[1:]
+}
---
+++ greed_test.go
@@ -0,0 +1,26 @@
+package greedgo
+
+import (	
+	"testing"
+)
+
+
+func TestGreed(t *testing.T) {
+	cases := []struct {expected int; input []int} {	
+		{1000, []int{1, 1, 1, 6, 6,}},
+		{1200, []int{1, 1, 1, 1, 1,}},
+		{1100, []int{1, 1, 1, 5, 5,}},	
+		{ 200, []int{2, 4, 2, 2, 3,}},
+		{ 250, []int{2, 5, 2, 2, 3,}},			
+		{ 550, []int{5, 5, 5, 5, 3,}},	
+		{ 300, []int{1, 5, 5, 1, 6,}},	
+
+	}
+
+	for _, aCase := range cases {
+		actual := Greed(aCase.input)
+		if actual != aCase.expected {
+			t.Errorf("Greed(%d) == %d, expected %d", aCase.input, actual, aCase.expected)
+		}
+	}
+}
